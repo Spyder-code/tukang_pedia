8e6c6e2177ff962925007fd945df2956c13170f0
Spyder-code <luaysyauqillah@gmail.com>
2023-12-22 09:00:29 +0700

    update register

--- resources/views/user/mitra.blade.php
+++ resources/views/user/mitra.blade.php
@@ -26,7 +26,7 @@
                         </div>
                         <div class="my-1 pr-4">
                             <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">NIK <sup class="text-red-400">*</sup></label>
-                            <input type="text" name="{{ old('nik') }}" required id="base-input" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <input type="text" name="nik" value="{{ old('nik') }}" required id="base-input" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         </div>
                     </div>
                     <div class="grid grid-cols-2 my-2">
